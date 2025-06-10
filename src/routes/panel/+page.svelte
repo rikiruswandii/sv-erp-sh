17a0aa2a32df57e7564d8823203941210a392b6a
Riki Ruswandi <riki@pratamatechsolution.co.id>
2025-06-10 09:51:37 +0700

    feat:panel routes

---
+++ src/routes/panel/+page.svelte
@@ -0,0 +1,13 @@
+<script lang='ts'>
+	import { enhance } from '$app/forms';
+	import type { PageServerData } from './$types';
+
+	let { data }: { data: PageServerData } = $props();
+    console.log(data);
+</script>
+
+<h1>Hi, {data.user.username}!</h1> 
+<p>Your user ID is {data.user.id}.</p>
+<form method='post' action='?/logout' use:enhance>
+	<button>Sign out</button>
+</form>
