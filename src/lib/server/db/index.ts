806557e089fa4298ad4fa1b19356a25fe79e5d78
Riki Ruswandi <riki@pratamatechsolution.co.id>
2025-06-12 17:45:32 +0700

    feat:use dotenv

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,10 +1,10 @@
 import { drizzle } from 'drizzle-orm/mysql2';
 import mysql from 'mysql2/promise';
 import * as schema from './schema';
-import { env } from '$env/dynamic/private';
+import 'dotenv/config';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
-const client = mysql.createPool(env.DATABASE_URL);
+const client = mysql.createPool(process.env.DATABASE_URL);
 
 export const db = drizzle(client, { schema, mode: 'default' });
