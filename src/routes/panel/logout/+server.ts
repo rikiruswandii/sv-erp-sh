f767cce6705d6fa6eedf948c25b9d3749adaef67
Riki Ruswandi <riki@pratamatechsolution.co.id>
2025-06-16 10:26:16 +0700

    feat:logout route

---
+++ src/routes/panel/logout/+server.ts
@@ -0,0 +1,14 @@
+import * as auth from '$lib/server/auth';
+import { redirect } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+
+export const POST: RequestHandler = async (event) => {
+  if (!event.locals.session) {
+    return new Response(null, { status: 401 });
+  }
+
+  await auth.invalidateSession(event.locals.session.id);
+  auth.deleteSessionTokenCookie(event);
+
+  throw redirect(302, '/');
+};
