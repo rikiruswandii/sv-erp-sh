c91dae20c368dfc6291794996210f0e31047e7e1
Riki Ruswandi <riki@pratamatechsolution.co.id>
2025-06-16 10:23:24 +0700

    feat:load server data

---
+++ src/routes/panel/+layout.server.ts
@@ -0,0 +1,19 @@
+// Deskripsi: Halaman ini digunakan untuk mengelola layout server pada panel admin.
+import { redirect } from '@sveltejs/kit';
+import type { LayoutServerLoad } from './$types';
+import {useInitials} from '$lib/hooks/useInitials';
+
+export const load: LayoutServerLoad = async (event) => {
+  const user = requireLogin(event);
+  const ak = useInitials(user.name);
+  return { user: { ...user, ak } };
+};
+
+// Memeriksa apakah user sudah login
+function requireLogin(event: Parameters<LayoutServerLoad>[0]) {
+  if (!event.locals.user) {
+    throw redirect(302, '/auth/login');
+  }
+
+  return event.locals.user;
+}
