55c96edb76353621174f7300340adbe8c259c554
Riki Ruswandi <riki@pratamatechsolution.co.id>
2025-06-20 16:11:49 +0700

    feat:more types

---
+++ src/lib/types/index.d.ts
@@ -0,0 +1,59 @@
+import type { SvelteComponent } from 'svelte';
+import type { Config } from 'ziggy-js';
+
+export interface Auth {
+  user: User;
+}
+
+export interface BreadcrumbItem {
+  title: string;
+  href: string;
+}
+
+export interface NavGroup {
+  title: string;
+  items: NavItem[];
+}
+
+export interface NavItem {
+  title: string;
+  href: string;
+  icon?: typeof SvelteComponent | null;
+  isActive?: boolean;
+}
+
+export interface SharedData {
+  name: string;
+  quote: { message: string; author: string };
+  auth: Auth;
+  ziggy: Config & { location: string };
+  sidebarOpen: boolean;
+  [key: string]: unknown;
+}
+
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  avatar?: string;
+  email_verified_at: string | null;
+  created_at: string;
+  updated_at: string;
+  [key: string]: unknown;
+}
+
+export type BreadcrumbItem =
+  | {
+      type: 'link';
+      href: string;
+      label: string;
+    }
+  | {
+      type: 'page';
+      label: string;
+    }
+  | {
+      type: 'dropdown';
+      icon?: boolean;
+      items: string[];
+    };
