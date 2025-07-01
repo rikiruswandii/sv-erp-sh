2460d84fc90e157ee4c041f4049415fc492cd094
rikiruswandii <rikiruswandi28@gmail.com>
2025-07-01 22:19:03 +0700

    feat:asset image

---
+++ src/lib/assets/index.ts
@@ -0,0 +1,10 @@
+import Mikeu from './mikeu.jpg'
+import Fan from './fan.jpg'
+import Fin from './fin.jpg'
+import Agoy from './agoy.jpg'
+import PakFeb from './pak_feb.jpg'
+import TehPut from './teh_put.jpg'
+import Zack from './zack.jpg'
+import Dion from './dion.jpg'
+
+export {Mikeu, Fan, Fin, Agoy, PakFeb, TehPut, Zack, Dion}
