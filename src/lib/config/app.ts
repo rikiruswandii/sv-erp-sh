287e7c963707f501ee1be1a46039e54a8f386d54
rikiruswandii <rikiruswandi28@gmail.com>
2025-07-01 22:21:57 +0700

    feat:component config global

---
+++ src/lib/config/app.ts
@@ -0,0 +1,21 @@
+export const APP_CONFIG = {
+	app_name: 'PT Pratama Solusi Teknologi',
+	address: 'Jl. Rusa I No. 57, Nagri Kidul, Purwakarta, Jawa Barat 41111 Indonesia',
+	phone: '+6285 72217 0064',
+	email: 'office@pratamatechsolution.co.id',
+	copyright: '© 2025 PT Pratama Solusi Teknologi',
+	website: 'www.pratamatechsolution.co.id',
+	about: `Pratama Solusi Teknologi merupakan
+perusahaan perseroan terbatas yang
+bergerak di bidang teknologi informasi,
+multimedia, advertising, dan event
+organizer.
+Saat ini kami telah melayani lebih dari
+200 klien di seluruh Indonesia. Seiring
+dengan perkembangan, kami berinisiatif
+untuk mengelompokkan seluruh produk
+dan layanan ke dalam suatu sistem yang
+terintegrasi untuk mewujudkan Smart
+City, Smart Company, dan Smart
+Goverment.`
+};
