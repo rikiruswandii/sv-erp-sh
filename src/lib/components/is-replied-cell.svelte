194648ddd8d47b4220ba84fd7ba09866614b4680
rikiruswandii <rikiruswandi28@gmail.com>
2025-07-02 23:31:50 +0700

    feat:component ui is replied cell

---
+++ src/lib/components/is-replied-cell.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import CheckCircle from '@lucide/svelte/icons/check-circle';
+	import CircleDot from '@lucide/svelte/icons/circle-dot';
+	import { Badge } from '$lib/components/ui/badge/index.js';
+
+	let { isReplied } = $props();
+</script>
+
+<div class="flex w-[120px] items-center">
+	{#if isReplied}
+		<Badge variant="outline"><CheckCircle class="size-4 text-green-500" />Replied</Badge>
+	{:else}
+		<Badge variant="outline"><CircleDot class="size-4 text-yellow-500" />Unreplied</Badge>
+	{/if}
+</div>
