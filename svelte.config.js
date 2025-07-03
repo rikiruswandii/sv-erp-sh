dc422630e87fb9e280d3b6299ce7b1001399ddd8
rikiruswandii <rikiruswandi28@gmail.com>
2025-07-03 08:20:28 +0700

    feat:change adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 // svelte.config.ts
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
