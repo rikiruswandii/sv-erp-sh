d220e8006ffd4abc949297f28050366bf082ab58
rikiruswandii <rikiruswandi28@gmail.com>
2025-07-03 15:18:35 +0700

    feat:modified

--- src/lib/types/index.d.ts
+++ src/lib/types/index.d.ts
@@ -52,6 +52,16 @@ export interface Page {
   updated_at: string;
   [key: string]: unknown;
 }
+export interface Setting {
+  id: string;
+  group: string;
+  key: string;
+  value: string;
+  description: string;
+  created_at: string;
+  updated_at: string;
+  [key: string]: unknown;
+}
 export interface ContactMessage {
   id: string;
   firstName: string;
