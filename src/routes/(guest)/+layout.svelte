e89921a4556e1f78ac6ea3df2a080f79e4c6be82
rikiruswandii <rikiruswandi28@gmail.com>
2025-07-03 10:45:47 +0700

    feat:loading overlay

--- src/routes/(guest)/+layout.svelte
+++ src/routes/(guest)/+layout.svelte
@@ -3,8 +3,28 @@
 	import { ModeWatcher } from 'mode-watcher';
 	import { Toaster } from '$lib/components/ui/sonner/index.js';
 	let { children } = $props();
+	import { afterNavigate, beforeNavigate } from '$app/navigation';
+	import { onMount } from 'svelte';
+	import LoaderCircle from '@lucide/svelte/icons/loader-circle';
+	let isLoading = $state(false);
+
+	onMount(() => {
+		beforeNavigate(() => {
+			isLoading = true;
+		});
+
+		afterNavigate(() => {
+			isLoading = false;
+		});
+	});
 </script>
 
 <ModeWatcher />
 <Toaster position="top-center" />
 {@render children()}
+
+{#if isLoading}
+	<div class="fixed inset-0 z-50 flex items-center justify-center bg-white/50 backdrop-blur-sm">
+		<LoaderCircle class="h-8 w-auto animate-spin text-blue-500" />
+	</div>
+{/if}
